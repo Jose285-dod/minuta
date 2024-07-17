641301ad87ab4e3cb972660715aa45a90562e9ab
jose hurtado <pitbullmck@gmail.com>
2024-07-17 11:19:42 -0500

    anexo jose

--- python1.py
+++ python1.py
@@ -1,1 +1,10 @@
 #"minuta"
+# Definir dos números para sumar
+numero1 = (a) 
+numero2 = (b)
+
+# Realizar la suma
+suma = numero1 + numero2
+
+# Mostrar el resultado
+print("La suma de", numero1, "y", numero2, "es:", suma)
